feda1dae4fadb48e78ccca12ccb5cf297307e1ac
yeosiowvic <victoryeo1974@gmail.com>
2022-11-02 15:27:16 +0800

    prevent contract from being initialised for multiple times

--- contracts/Box.sol
+++ contracts/Box.sol
@@ -3,8 +3,11 @@ pragma solidity 0.8.17;
 
 contract Box {
   uint public val;
+  bool private initialized;
 
   function initialize(uint _val) external {
+    require(!initialized, "Contract instance has already been initialized");
+    initialized = true;
     val = _val;
   }
 }
