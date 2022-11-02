f0ceef0476bc959b07dece27eb14a7082a2c4d51
yeosiowvic <victoryeo1974@gmail.com>
2022-11-02 10:12:14 +0800

    hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,4 +3,14 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+    goerli: {
+      chainId: 5,
+      url: `https://goerli.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY
+  }
 };
