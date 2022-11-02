ac5d9170dfc6b38007abd0f7ad997afd918f8d3d
yeosiowvic <victoryeo1974@gmail.com>
2022-11-02 15:43:55 +0800

    invoke the _disableInitializers function in the constructor to automatically lock implementation contract

--- contracts/Box.sol
+++ contracts/Box.sol
@@ -1,9 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.17;
 
-contract Box {
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+
+contract Box is Initializable {
   uint public val;
   bool private initialized;
+
+  /// @custom:oz-upgrades-unsafe-allow constructor
+  constructor() {
+      _disableInitializers();
+  }
 
   function initialize(uint _val) external {
     require(!initialized, "Contract instance has already been initialized");
